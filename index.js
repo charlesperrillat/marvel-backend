37a2c75b6f172754a8055957057fa85ec0019eac
charlesperrillat <ce.perrillat@gmail.com>
2024-09-08 22:46:10 +0200

    commented the database variable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
 
 const characterRoutes = require("./routes/character");
 const comicsRoutes = require("./routes/comics");
